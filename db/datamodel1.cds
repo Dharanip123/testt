5e8505c54900600d895ac9b522fc598d9abb0d55
Dharani Penta <pentadharani800@gmail.com>
2024-08-21 10:24:09 +0000

    capm

--- db/datamodel1.cds
+++ db/datamodel1.cds
@@ -54,19 +54,19 @@ context master {
 }
 
 context transaction {
-    entity purchaseorder: common.Amount{
-        key NODE_KEY: common.Guid;
-        PO_ID: String(40); 
-        PARTNER_GUID: Association to master.businesspartner;
+    entity purchaseorder: cuid , common.Amount {
+        // key NODE_KEY: common.Guid;
+        PO_ID: String(40) @title : '{i18n>PO_ID}';
+        PARTNER_GUID: Association to master.businesspartner @title : '{i18n>PARTNER_KEY}';
         LIFECYCLE_STATUS: String(1);
-        OVERALL_STATUS: String(1);
-        Items: Composition of many poitems on Items.PARENT_KEY = $self;
+        OVERALL_STATUS: String(1) @title : '{i18n>STATUS}';
+        Items: Composition of  many poitems on Items.PARENT_KEY = $self;
     }
 
-    entity poitems: common.Amount{
-        key NODE_KEY: common.Guid;
-        PARENT_KEY: Association to purchaseorder;
-        PO_ITEM_POS: Integer;
-        PRODUCT_GUID: Association to master.product;
+    entity poitems: cuid , common.Amount {
+        // key NODE_KEY: common.Guid;
+        PARENT_KEY: Association to purchaseorder @title : '{i18n>PO_NODE_KEY}';
+        PO_ITEM_POS: Integer @title : '{i18n>ITEM_POS}';
+        PRODUCT_GUID: Association to master.product @title : '{i18n>PRODUCT_ID}';
     }
 }
--- srv/EmployeeService.js
+++ srv/EmployeeService.js
@@ -1,7 +1,7 @@
 module.exports = cds.service.impl( async function(){
  
     //Step 1: get the object of our odata entities
-    const { EmployeeSet} = this.entities;
+    const { EmployeeSet } = this.entities;
 
     this.before('CREATE', EmployeeSet, (req) => {
         var salary = parseInt(req.data.salaryAmount);
@@ -11,27 +11,46 @@ module.exports = cds.service.impl( async function(){
             console.log("Employees salary must be less than 50000 USD");
         }
     });
+    this.before('CREATE', EmployeeSet, (req) => {
+        var salary = parseInt(req.data.salaryAmount);
+        var currencycode = req.data.Currency_code
+        if(currencycode != 'USD'){
+            req.error("Employee’s salary must be less than 50000 USD");
+        console.log("Employee’s salary must be less than 50000 USD");
+        }
+    });
     
-    // this.before ('UPDATE', EmployeeSet, async (req) => { if ('nameFirst' in req.data) {
-    //     req.error("Operation not allowed");
+    this.before ('UPDATE', EmployeeSet, async (req) => { if ('nameFirst' in req.data) {
+        req.error("Operation not allowed");
        
-    // }})
-    // this.before ('UPDATE', EmployeeSet, async (req) => { if ('loginName' in req.data) {
-    //     req.error("Operation not allowed");
+    }})
+
+    this.before ('UPDATE', EmployeeSet, async (req) => { if ('loginName' in req.data) {
+        req.error("Operation not allowed");
        
-    // }})
+    }})
  
-    // this.on('CREATE' , EmployeeSet, (req) => {
-    //     console.log("Create operation successfull")
-    // })
+    this.on('CREATE' , EmployeeSet, (req) => {
+        console.log("Create operation successfull")
+    })
  
-    // this.on('DELETE' , EmployeeSet, (req) => {
-    //     console.log("Delete operation successfull")
-    // })
+    this.on('DELETE' , EmployeeSet, (req) => {
+        console.log("Delete operation successfull")
+    })
  
-    // this.on('UPDATE' , EmployeeSet, (req) => {
-    //     console.log("Delete operation successfull")
-    // })
+    this.on('UPDATE' , EmployeeSet, (req) => {
+        console.log("Delete operation successfull")
+    })
+
+    // this.before('DELETE', EmployeeSet, async (req) => {
+ 
+    //     let id=req.data.ID;
+    //     const pastData = await SELECT.from(EmployeeSet).columns('nameFirst').where({ID:id});
+    //     console.log(pastData[0].nameFirst[0]);
+    //     if(pastData[0].nameFirst[0] == 'S'){
+    //         req.error('Operation Not Possible');
+    //     }
+    // });
  
 }
 );
